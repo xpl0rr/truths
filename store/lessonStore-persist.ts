f8a6e0945677843b92a0900747cbbdd9e5a57a49
DougDelaney <xplorr@pm.me>
2025-05-03 17:53:16 -0700

    Switch from react-native-mmkv to expo-mmkv for storage implementation

--- store/lessonStore-persist.ts
+++ store/lessonStore-persist.ts
@@ -1,5 +1,5 @@
 import { create } from 'zustand';
-import { MMKV } from 'react-native-mmkv';
+import { MMKV } from 'expo-mmkv';
 import { useEffect } from 'react';
 
 // Use MMKV for faster, more reliable on-device persistence
