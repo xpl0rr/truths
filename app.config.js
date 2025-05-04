5b0bddc8c9e74ce4e13c0adb78c354d750329c3c
DougDelaney <xplorr@pm.me>
2025-05-03 18:21:57 -0700

    Add expo-dev-client plugin to app config

--- app.config.js
+++ app.config.js
@@ -26,7 +26,7 @@ module.exports = {
     },
     web: { favicon: './assets/images/favicon.png' },
     splash: { image: './assets/images/splash-icon.png', resizeMode: 'contain', backgroundColor: '#ffffff' },
-    plugins: ['expo-router'],
+    plugins: ['expo-router', 'expo-dev-client'],
     experiments: { typedRoutes: true },
     updates: { url: 'https://u.expo.dev/4137856e-1f8e-436a-8b2f-3e0fb7f94b4a' },
     extra: { eas: { projectId: '4137856e-1f8e-436a-8b2f-3e0fb7f94b4a' } },
