ecadcfc93afdc1c34cf44cfa33c8c3ed6cf88eca
DougDelaney <xplorr@pm.me>
2025-05-06 11:22:21 -0700

    Update import path for lessonStore from persist to non-persist version

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -11,7 +11,7 @@ import {
   Alert,
 } from 'react-native';
 import textStyles from '../styles/textStyles';
-import { useLessons } from '../../store/lessonStore-persist';
+import { useLessons } from '../../store/lessonStore';
 import type { Lesson } from '../../src/models/Lesson';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import AddTruthModal from '../components/AddTruthModal';
